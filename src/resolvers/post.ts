6ace7ccf98d3aa3f1833afe3ea6768de22836387
mynsputo <mynspluto@gmail.com>
2021-10-17 03:54:12 +0900

    .

---
+++ src/resolvers/post.ts
@@ -0,0 +1,18 @@
+import { Post } from '../entities/Post';
+import { Resolver, Query, Ctx } from 'type-graphql';
+import { MyContext } from 'src/types';
+
+@Resolver()
+export class PostResolver {
+  @Query(() => String)
+  hi() {
+    return "e"
+  }
+  
+  @Query(() => [Post])
+  posts(@Ctx() { em } : MyContext): Promise<Post[]> { 
+    
+    return em.find(Post, {});
+  }
+   
+} 
